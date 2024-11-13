8973ba5e9169947ed912f0a02e37c84df15908af
Xavier <xvr0612@gmail.com>
2024-11-13 21:53:20 +0000

    optomized and commented code

--- projects/1. Socket Programming/http_client.c
+++ projects/1. Socket Programming/http_client.c
@@ -8,72 +8,62 @@
 
 #define BUFFER_SIZE 4096
 
+// Print usage information
 void print_usage(const char *prog_name) {
     printf("Usage: %s [-p] server_url port_number\n", prog_name);
 }
 
+// Calculate round-trip time (RTT) in milliseconds
 long calculate_rtt(struct timeval start, struct timeval end) {
-    long seconds = end.tv_sec - start.tv_sec;
-    long microseconds = end.tv_usec - start.tv_usec;
-    return (seconds * 1000) + (microseconds / 1000);  // return RTT in milliseconds
+    return ((end.tv_sec - start.tv_sec) * 1000) + ((end.tv_usec - start.tv_usec) / 1000);
 }
 
 int main(int argc, char *argv[]) {
-    int sock, port, rtt_flag = 0;
-    struct sockaddr_in server_addr;
-    struct hostent *server;
-    char request[BUFFER_SIZE], response[BUFFER_SIZE];
-    struct timeval start, end;
-
-    // Argument parsing
+    // Check for correct number of arguments
     if (argc < 3 || argc > 4) {
         print_usage(argv[0]);
         return 1;
     }
 
-    if (argc == 4 && strcmp(argv[1], "-p") == 0) {
-        rtt_flag = 1;
-        argv++;  // shift arguments to treat server_url as argv[1] and port as argv[2]
-    }
+    // Check if RTT flag is set
+    int rtt_flag = (argc == 4 && strcmp(argv[1], "-p") == 0);
+    if (rtt_flag) argv++;
 
     char *server_url = argv[1];
-    port = atoi(argv[2]);
+    int port = atoi(argv[2]);
 
-    // Split server_url into hostname and path
+    // Extract path from URL
     char *path = strchr(server_url, '/');
     if (path) {
-        *path = '\0';  // Null-terminate the hostname part
-        path++;        // Move to the start of the path
+        *path++ = '\0';
     } else {
-        path = "/";  // Default to root if no path is provided
+        path = "/";
     }
 
-    // Resolve server address
-    server = gethostbyname(server_url);
-    if (server == NULL) {
+    // Get server address
+    struct hostent *server = gethostbyname(server_url);
+    if (!server) {
         fprintf(stderr, "ERROR: No such host\n");
         return 1;
     }
 
     // Create socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("ERROR: Unable to open socket");
         return 1;
     }
 
-    // Prepare server address structure
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
+    // Set up server address structure
+    struct sockaddr_in server_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(port)};
     memcpy(&server_addr.sin_addr.s_addr, server->h_addr_list[0], server->h_length);
 
-    // Measure RTT if `-p` option is enabled
-    if (rtt_flag) {
-        gettimeofday(&start, NULL);
-    }
+    struct timeval start, end;
+    if (rtt_flag) gettimeofday(&start, NULL);
 
-    // Connect to the server
+    // Connect to server
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("ERROR: Connection failed");
         close(sock);
@@ -82,34 +72,33 @@ int main(int argc, char *argv[]) {
 
     if (rtt_flag) {
         gettimeofday(&end, NULL);
-        long rtt = calculate_rtt(start, end);
-        printf("RTT: %ld ms\n", rtt);
+        printf("RTT: %ld ms\n", calculate_rtt(start, end));
     }
 
-    // Formulate HTTP GET request
-    snprintf(request, sizeof(request),
-             "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", path, server_url);
+    // Create HTTP GET request
+    char request[BUFFER_SIZE];
+    snprintf(request, sizeof(request), "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", path, server_url);
 
-    // Send the HTTP GET request
+    // Send request
     if (send(sock, request, strlen(request), 0) < 0) {
         perror("ERROR: Failed to send request");
         close(sock);
         return 1;
     }
 
-    // Receive and display the response
+    // Receive response
+    char response[BUFFER_SIZE];
     ssize_t bytes_received;
     while ((bytes_received = recv(sock, response, sizeof(response) - 1, 0)) > 0) {
-        response[bytes_received] = '\0';  // Null-terminate the response
-        printf("%s", response);           // Print the response to stdout
+        response[bytes_received] = '\0';
+        printf("%s", response);
     }
 
     if (bytes_received < 0) {
         perror("ERROR: Failed to receive response");
     }
 
-    printf("\n");
-    // Clean up
+    // Close socket
     close(sock);
     return 0;
 }
--- projects/1. Socket Programming/http_server.c
+++ projects/1. Socket Programming/http_server.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 1048576 // roughly 1MB
@@ -18,30 +17,29 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
 
-    // Get port number from command line or use default
+    // Set port to user-specified or default
     port = (argc == 2) ? atoi(argv[1]) : DEFAULT_PORT;
 
-    // Create server socket
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock < 0) {
+    // Create socket
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("ERROR: Unable to open socket");
         return 1;
     }
 
-    // Set up the server address structure
+    // Initialize server address structure
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
-    // Bind the socket to the specified port
+    // Bind socket to address and port
     if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("ERROR: Unable to bind");
         close(server_sock);
         return 1;
     }
 
-    // Start listening for connections
+    // Listen for incoming connections
     if (listen(server_sock, 10) < 0) {
         perror("ERROR: Unable to listen");
         close(server_sock);
@@ -52,46 +50,41 @@ int main(int argc, char *argv[]) {
 
     // Accept and handle incoming connections
     while (1) {
-        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_len);
-        if (client_sock < 0) {
+        if ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_len)) < 0) {
             perror("ERROR: Unable to accept connection");
             continue;
         }
 
         handle_client(client_sock);
-        close(client_sock);  // Close the connection after handling
+        close(client_sock);
     }
 
-    // Close the server socket (not reached due to infinite loop, unless interrupted)
     close(server_sock);
     return 0;
 }
 
 void handle_client(int client_sock) {
-    char buffer[BUFFER_SIZE];
-    memset(buffer, 0, BUFFER_SIZE);
+    char buffer[BUFFER_SIZE] = {0};
 
-    // Read the HTTP request from the client
+    // Receive request from client
     int bytes_received = recv(client_sock, buffer, BUFFER_SIZE - 1, 0);
     if (bytes_received < 0) {
         perror("ERROR: Failed to read from socket");
         return;
     }
 
-    // Parse the HTTP request to check if it's a GET request
+    // Handle GET request
     if (strncmp(buffer, "GET ", 4) == 0) {
-        // Get the requested file name from the GET request
         char *file_name = strtok(buffer + 4, " ");
-        if (file_name[0] == '/') file_name++;  // Remove leading '/'
+        if (file_name[0] == '/') file_name++;
 
-        // Default to index.html if no file is specified
         if (strlen(file_name) == 0) {
             file_name = "index.html";
         }
 
         send_file_response(client_sock, file_name);
     } else {
-        // Send a 400 Bad Request response for non-GET requests
+        // Send 400 Bad Request response for unsupported methods
         send_response(client_sock, "400 Bad Request", "text/plain", "Only GET method is supported.");
     }
 }
@@ -100,6 +93,7 @@ void send_response(int client_sock, const char *status, const char *content_type
     char response[BUFFER_SIZE];
     int body_length = strlen(body);
 
+    // Format and send HTTP response
     snprintf(response, sizeof(response),
              "HTTP/1.1 %s\r\n"
              "Content-Type: %s\r\n"
@@ -113,19 +107,20 @@ void send_response(int client_sock, const char *status, const char *content_type
 }
 
 void send_file_response(int client_sock, const char *filename) {
+    // Open requested file
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        // Send 404 Not Found response if the file does not exist
+        // Send 404 Not Found response if file doesn't exist
         send_response(client_sock, "404 Not Found", "text/plain", "404 Not Found");
         return;
     }
 
-    // Get the file contents
+    // Read file content
     char file_content[BUFFER_SIZE];
     size_t bytes_read = fread(file_content, 1, sizeof(file_content) - 1, file);
-    file_content[bytes_read] = '\0';  // Null-terminate the file content
+    file_content[bytes_read] = '\0';
     fclose(file);
 
-    // Send the file content as HTTP response
+    // Send file content as HTTP response
     send_response(client_sock, "200 OK", "text/html", file_content);
 }
